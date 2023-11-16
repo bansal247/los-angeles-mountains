2bf59fb82e7ddae47f65f04df96a807d43db4f3d
Shashwat Bansal <66252519+bansal247@users.noreply.github.com>
2023-11-17 01:23:12 +0530

    completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaBars } from "react-icons/fa";
 
 import Logo from '../assets/Logo.png'
 import LogoLosAngeles from '../assets/LogoLosAngeles.png'
 import styles from '../styles/Header.module.css'
 
-function Header({handleClick,isContent = true}) {
+function Header({ handleClick, isContent = true }) {
+  const [bar, setBar] = useState(false)
+  const handleClickBar = () => {
+    setBar(!bar)
+  }
   return (
-    <div className={`${styles.main} ${isContent ? styles.mainContent: null}`}>
+    <div>
+      <div className={`${styles.main} ${isContent ? styles.mainContent : null}`}>
         <div className={styles.left}>
-        {isContent ? <img src={LogoLosAngeles} alt="logo" /> : <img src={Logo} alt="logo" /> }
+          {isContent ? <img src={LogoLosAngeles} alt="logo" /> : <img src={Logo} alt="logo" />}
         </div>
-        <div className={`${styles.right} ${isContent ? styles.nonHero: null}`}>
-        <div style={{cursor:'pointer'}} onClick={handleClick} id='history'>01. HISTORY</div>
-        <div style={{cursor:'pointer'}} onClick={handleClick} id='team'>02. TEAM</div>
+        <div onClick={handleClickBar} className={styles.bar}>
+          <FaBars size={20} color='#414f6b' />
         </div>
 
+        <div className={`${styles.right} ${isContent ? styles.nonHero : null}`}>
+          <div style={{ cursor: 'pointer' }} onClick={handleClick} id='history'>01. HISTORY</div>
+          <div style={{ cursor: 'pointer' }} onClick={handleClick} id='team'>02. TEAM</div>
+        </div>
+
+      </div>
+      <div style={bar ? { display: 'inline' } : { display: 'none' }} >
+        <div className={`${styles.rightBar} ${styles.nonHero}`}>
+          <div style={{ cursor: 'pointer', borderBlockEnd:'#414f6b 1px solid' , width:'100%',textAlign:'center' }} onClick={handleClick} id='history'>01. HISTORY</div>
+          <div style={{ cursor: 'pointer' , width:'100%',textAlign:'center' }} onClick={handleClick} id='team'>02. TEAM</div>
+        </div>
+      </div>
     </div>
   )
 }
